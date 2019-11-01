c1107c71bdd2b725598f98d938657012489f9352
Rodrigo Vieira <rcardoso@gmail.com>
2019-11-01 17:11:16 -0300

    Inclui o Deploy para o heroku no config.yml

--- start_docker.sh
+++ start_docker.sh
@@ -6,4 +6,4 @@ export PORT="8080"
 mvn clean package -DskipTests && \
 docker build -t helloworld-cicd:latest . && \
 #docker run --name helloworld-cicd --rm -d -p 8080:$PORT/tcp helloworld-cicd:latest
-docker run --name helloworld-cicd --rm -it -p 8080:$PORT/tcp helloworld-cicd:latest
+docker run --name helloworld-cicd --rm -it -e PORT -p 8080:$PORT/tcp helloworld-cicd:latest
