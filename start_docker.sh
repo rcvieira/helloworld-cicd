0942f5e275a506018d8f905044b12cfc6824ac26
Rodrigo Vieira <rcardoso@gmail.com>
2019-11-01 16:43:23 -0300

    Corrige o Dockerfile e start_docker.sh para rodar no heroku. Seta a 8000

--- start_docker.sh
+++ start_docker.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+export PORT="8080"
+
 mvn clean package -DskipTests && \
 docker build -t helloworld-cicd:latest . && \
-docker run --name helloworld-cicd --rm -d -p 8080:8080/tcp helloworld-cicd:latest
+#docker run --name helloworld-cicd --rm -d -p 8080:$PORT/tcp helloworld-cicd:latest
+docker run --name helloworld-cicd --rm -it -p 8080:$PORT/tcp helloworld-cicd:latest
